2fb62e8d1f5db7306097e08f6b9bfc05b9886e13
Nikola <anrakee@gmail.com>
2014-01-03 02:49:01 +0100

    Skoro pa skoro

--- script.js
+++ script.js
@@ -1,62 +1,146 @@
-document.onkeydown = checkKey;
+var frameWidth = 236;
+var taskMin = 5000;
+var taskMax = 6000;
 
-function checkKey(e) {
-    e = e || window.event;
-    checkJopaControls(e.keyCode);
+var sounds = {
+  coin: new Audio("http://www.basementarcade.com/arcade/sounds/domman/coin%20up.wav")
 }
 
-var frameWidth = 236;
+var jopa = {
+  name: "Jopa",
+  task: null,
+  state: ["idle", 0, 0],
+  frames: {
+    "idle": [[0]],
+    "left": [[1, 2, 3, 2], [4, 5], [6, 7]],
+    "right":[[8, 9], [10, 11, 12, 11], [13, 14]],
+    "up":   [[15, 16, 17, 16], [18, 19], [21, 20]],
+    "down": [[22, 23], [24, 25], [26, 27]]
+  },
+  controls: {"38": "up", "37": 'left', "40": 'down', "39": 'right'},
+  area: function() { return $("#jopa"); },
+  sprite: function() { return $("#jopa .player"); }
+};
 
-var jopaState = [0, 0];
-var jopaFrames = [[0], [1, 2, 3, 2], [4, 5], [6, 7], [8, 9], [10, 11, 12, 11], 
-                  [13, 14], [15, 16, 17, 16], [18, 19], [21, 20], [22, 23], [24, 25], [26, 27]];
-                  
-var asjaState = [0, 0];
-var asjaFrames = [];
+var asja = {
+  name: "Asja",
+  task: null,
+  state: ["idle", 0, 0],
+  frames: {
+    "idle": [[0, 2]],
+    "left": [[1, 3], [1, 3, 4, 5]], 
+    "right":[[1, 3, 6, 7], [6, 7, 8]],
+    "up":   [[9, 10], [11, 12]],
+    "down": [[12, 13, 12, 14], [15, 16]]
+  },
+  controls: {"87": "up", "65": 'left', "83": 'down', "68": 'right'},
+  area: function() { return $("#asja"); },
+  sprite: function() { return $("#asja .player"); }
+};
 
 $(function() {
-  var $jopa = $("#jopa");
-  var $asja = $("#asja");
-  
-  var frameInterval = setInterval(function() {
-    setFrame("jopa", $jopa, jopaState, jopaFrames);
-    //setFrame("asja", $asja, asjaState, asjaFrames);
-  }, 50);
-  
-  var animateInterval = setInterval(function() {
-    jopaState = giveOompf(jopaState);
-    //asjaState = giveOompf(asjaState);
-  }, 300);
-  
+  titleScreen();
+  //gameScreen();
 });
 
-function setFrame(name, $player, state, frames) {
-  var frameset = frames[state[0]];
-  state[1] = state[1] % frameset.length;
-  var frame = frameset[state[1]];
-  $player.css('background-position', -frameWidth * frame + "px 0px");
+function titleScreen() {
+  $("#title-screen").show();
+  $("#start-game").click(function() {
+    sounds["coin"].play();
+    $("#title-screen").fadeOut(1000, gameScreen);
+  });
 }
 
-function giveOompf(state, frame) {
-  state[1] = state[1] + 1
-  return state;
+function gameScreen() {
+  $("#game-screen").show();
+  $(".player-area .name").removeClass("hidden");
+  
+  showWithFlicker([asja.sprite(), jopa.sprite()], 7, function() {
+    startCountdown(3);
+  });
 }
 
-function checkJopaControls(key) {
-  // Svatka tipka ciklira između 3 različita poteza
-  /* w */ if (key == '87')      { jopaState[0] = jopaState[0] % 3 + 1}
-  /* a */ else if (key == '65') { jopaState[0] = jopaState[0] % 3 + 4}
-  /* s */ else if (key == '83') { jopaState[0] = jopaState[0] % 3 + 7}
-  /* d */ else if (key == '68') { jopaState[0] = jopaState[0] % 3 + 10}
-  console.log(jopaState[0])
+function showWithFlicker($elements, count, onSuccess) {
+  if (count <= 0) { $elements.shift(); count = 7;}
+  if ($elements.length == 0) { return onSuccess(); }
+  
+  var $element = $elements[0];
+  setTimeout(function() { 
+    $element.toggleClass("hidden");
+    showWithFlicker($elements, count - 1, onSuccess)
+  }, Math.random() * count * 100);
 }
 
-function checkJopaControls2(key) {
-  /* w */ if (key == '87')      { jopaState[0] = jopaState[0] % 12 + 1}
-  /* s */ else if (key == '83') { jopaState[0] = (jopaState[0] + 11) % 12}
-  /* a */ else if (key == '65') { jopaState[1] += 1 }
-  /* d */ else if (key == '68') { jopaState[1] += 1 }
+function startCountdown(count) {
+  var countdown = count;
+  var countdownInterval = setInterval(function() {
+    if (countdown < 0) { 
+      $("#countdown").hide();
+      clearInterval(countdownInterval); 
+      startGame();
+    } else {
+      $("#countdown").html(countdown || "Dance!");
+      countdown--;
+    }
+  }, 1000);
 }
 
+function startGame() {
+  document.onkeydown = checkKey;
+  
+  var frameInterval = setInterval(function() {
+    setFrame(jopa);
+    setFrame(asja);
+  }, 50);
+  
+  var animateInterval = setInterval(giveOompf, 300);
+  generateTask(jopa);
+  generateTask(asja);
+}
+
+function setFrame(player) {
+  var frameGroup = player.frames[player.state[0]];
+  player.state[1] = player.state[1] % frameGroup.length;
+  var frameset = frameGroup[player.state[1]];
+  player.state[2] = player.state[2] % frameset.length;
+  var frame = frameset[player.state[2]];
+  player.sprite().css('background-position', -frameWidth * frame + "px 0px");
+}
+
+function giveOompf(player) {
+  jopa.state[2] = jopa.state[2] + 1;
+  asja.state[2] = asja.state[2] + 1;
+}
+
+function generateTask(player) {
+  var task = ["left", "up", "down", "right"][Math.floor(Math.random() * 4)];
+  player.task = task;
+  player.area().find(".task").removeClass("true false").html(task);
+  setTimeout(function() { generateTask(player); }, Math.random() * (taskMax - taskMin) + taskMin);
+}
+
+function checkKey(e) {
+  e = e || window.event;
+  checkControls(jopa, e.keyCode);
+  checkControls(asja, e.keyCode);
+}
+
+function checkControls(player, key) {
+  var playerDirection = player.controls[key];
+  if (playerDirection == null) return;
+  
+  player.state[0] = playerDirection;
+  player.state[1] = player.state[1] + 1;
+  
+  if (player.task) {
+    var correct = (playerDirection == player.task);
+    player.area().find(".task").addClass(correct ? "true" : "false");    
+    player.task = null;
+  }
+    
+   
+  // Svatka tipka ciklira između grupe različitih poteza
+  
+}
 
 
