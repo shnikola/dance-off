d4c4fae8dc0a55f71d6d010f0016efa5b3c220ec
Nikola <anrakee@gmail.com>
2014-01-14 03:59:42 +0100

    Almost

---
+++ canvas.js
@@ -0,0 +1,70 @@
+var canvas = {}
+
+function initCanvas() {
+  canvas.element = document.getElementById("canvas");
+  canvas.ctx = canvas.element.getContext("2d");
+	canvas.W = window.innerWidth;
+	canvas.H = window.innerHeight;
+	canvas.element.width = canvas.W;
+	canvas.element.height = canvas.H;
+  canvas.max_particles = 100;
+  canvas.particles = [];
+  canvas.angle = 0;
+  
+	for (var i = 0; i < canvas.max_particles; i++) {
+		canvas.particles.push({
+			x: Math.random() * canvas.W, //x-coordinate
+			y: Math.random() * canvas.H, //y-coordinate
+			r: Math.random() * 4 + 1, //radius
+			d: Math.random() * canvas.max_particles, //density
+      color: "rgba(" + Math.floor((Math.random() * 255)) +", " + Math.floor((Math.random() * 255)) +", " + Math.floor((Math.random() * 255)) + ", 0.8)"});
+	}
+}
+
+function drawCanvas() {
+	canvas.ctx.clearRect(0, 0, canvas.W, canvas.H);
+	for (var i = 0; i < canvas.max_particles; i++) { 
+		var p = canvas.particles[i];
+    canvas.ctx.beginPath();
+    canvas.ctx.fillStyle = p.color;
+		canvas.ctx.fillRect(p.x, p.y, 6, 6);
+    canvas.ctx.fill();
+	}
+	
+	updateParticles();
+}
+
+//Function to move the snowflakes
+//angle will be an ongoing incremental flag. Sin and Cos functions will be applied to it to create vertical and horizontal movements of the flakes
+function updateParticles() {
+	canvas.angle += 0.01;
+	for (var i = 0; i < canvas.max_particles; i++) {
+		var p = canvas.particles[i];
+		//Updating X and Y coordinates
+		//We will add 1 to the cos function to prevent negative values which will lead flakes to move upwards
+		//Every particle has its own density which can be used to make the downward movement different for each flake
+		//Lets make it more random by adding in the radius
+		p.y += 2 * (Math.cos(canvas.angle+p.d) + 1 + p.r/2);
+		p.x += Math.sin(canvas.angle) * 2;
+		
+		//Sending flakes back from the top when it exits
+		//Lets make it a bit more organic and let flakes enter from the left and right also.
+		if (p.x > canvas.W+5 || p.x < -5 || p.y > canvas.H) {
+			if (i % 3 > 0) { //66.67% of the flakes
+        canvas.particles[i] = {x: Math.random() * canvas.W, y: -10, r: p.r, d: p.d, color : p.color};
+			} else if (Math.sin(canvas.angle) > 0) {
+				//Enter from the left
+        canvas.particles[i] = {x: -5, y: Math.random() * canvas.H, r: p.r, d: p.d, color: p.color};
+			} else {
+				//Enter from the right
+        canvas.particles[i] = {x: canvas.W + 5, y: Math.random() * canvas.H, r: p.r, d: p.d, color : p.color};
+			}
+		}
+	}
+}
+
+function startConfetti() {
+  initCanvas();
+	setInterval(drawCanvas, 33);
+}
+
--- script.js
+++ script.js
@@ -1,42 +1,69 @@
-var frameWidth = 236;
-var taskMin = 5000;
-var taskMax = 6000;
+var game = {
+  interacting: false,
+  animating: false,
+  animationInterval: 320,
+  backgroundInterval: 1000,
+  taskLength: [3000, 4000],
+  taskDifficulty: 0,
+  randomTaskLength: function() {
+    return Math.random() * (this.taskLength[1] - this.taskLength[0]) + this.taskLength[0];
+  },
+  reset: function() {
+    this.animationInterval = 320;
+    this.backgroundInterval = 1000;
+
+    this.taskLength = [3000, 4000];
+    this.taskDifficulty = 0;
+  }
+};
 
 var sounds = {
-  coin: new Audio("http://www.basementarcade.com/arcade/sounds/domman/coin%20up.wav")
-}
+  coin: new Audio("http://www.basementarcade.com/arcade/sounds/domman/coin%20up.wav"),
+  music: new Audio("tune1.mp3"),
+  bonus: new Audio("http://themushroomkingdom.net/sounds/wav/smb/smb_flagpole.wav"),
+  win: new Audio("http://themushroomkingdom.net/sounds/wav/smb/smb_world_clear.wav")
+};
 
 var jopa = {
   name: "Jopa",
   task: null,
+  points: 0,
   state: ["idle", 0, 0],
   frames: {
-    "idle": [[0]],
-    "left": [[1, 2, 3, 2], [4, 5], [6, 7]],
-    "right":[[8, 9], [10, 11, 12, 11], [13, 14]],
-    "up":   [[15, 16, 17, 16], [18, 19], [21, 20]],
-    "down": [[22, 23], [24, 25], [26, 27]]
+    "idle": [[0, 1]],
+    "left": [[2, 3, 4, 3], [5, 6], [7, 8]],
+    "right":[[9, 10], [11, 12, 13, 12], [14, 15]],
+    "up":   [[16, 17, 18, 17], [19, 20], [22, 21]],
+    "down": [[23, 24], [25, 26], [27, 28]],
+    "end":  [[29, 30, 31, 32, 33, 34]]
   },
+  frameWidth: 236,
   controls: {"38": "up", "37": 'left', "40": 'down', "39": 'right'},
   area: function() { return $("#jopa"); },
-  sprite: function() { return $("#jopa .player"); }
+  sprite: function() { return $("#jopa .player"); },
 };
 
 var asja = {
   name: "Asja",
   task: null,
+  points: 0,
   state: ["idle", 0, 0],
   frames: {
     "idle": [[0, 2]],
-    "left": [[1, 3], [1, 3, 4, 5]], 
-    "right":[[1, 3, 6, 7], [6, 7, 8]],
-    "up":   [[9, 10], [11, 12]],
-    "down": [[12, 13, 12, 14], [15, 16]]
+    "left": [[1, 3], [6, 7, 8]], 
+    "right":[[1, 3, 6, 7], [1, 3, 4, 5]],
+    "up":   [[11, 12], [15, 16]],
+    "down": [[12, 13, 12, 14], [9, 10]],
+    "end":  [[17]]
   },
+  frameWidth: 236,
   controls: {"87": "up", "65": 'left', "83": 'down', "68": 'right'},
   area: function() { return $("#asja"); },
   sprite: function() { return $("#asja .player"); }
 };
+
+
+// ======================== Game Setup ========================
 
 $(function() {
   titleScreen();
@@ -56,7 +83,9 @@ function gameScreen() {
   $(".player-area .name").removeClass("hidden");
   
   showWithFlicker([asja.sprite(), jopa.sprite()], 7, function() {
-    startCountdown(3);
+    sounds["music"].play();
+    startCountdown(5);
+    $(".player-area .controls").removeClass("hidden");
   });
 }
 
@@ -72,10 +101,11 @@ function showWithFlicker($elements, count, onSuccess) {
 }
 
 function startCountdown(count) {
+  $("#countdown").html("").removeClass("hidden")  
   var countdown = count;
   var countdownInterval = setInterval(function() {
     if (countdown < 0) { 
-      $("#countdown").hide();
+      $("#countdown").addClass("hidden");
       clearInterval(countdownInterval); 
       startGame();
     } else {
@@ -85,17 +115,26 @@ function startCountdown(count) {
   }, 1000);
 }
 
+
+// ======================== Game Play ========================
+
+
 function startGame() {
+  game.interacting = true;
+  game.animating = true;
   document.onkeydown = checkKey;
+  $(".player-area .controls").addClass("hidden");
+  $(".player-area .points").removeClass("hidden");
   
-  var frameInterval = setInterval(function() {
-    setFrame(jopa);
-    setFrame(asja);
-  }, 50);
-  
-  var animateInterval = setInterval(giveOompf, 300);
+  setFrame(jopa);
+  setFrame(asja);
+  animate();
+
   generateTask(jopa);
   generateTask(asja);
+  setHardening();
+
+  setTimeout(endGame, 115000);
 }
 
 function setFrame(player) {
@@ -104,19 +143,63 @@ function setFrame(player) {
   var frameset = frameGroup[player.state[1]];
   player.state[2] = player.state[2] % frameset.length;
   var frame = frameset[player.state[2]];
-  player.sprite().css('background-position', -frameWidth * frame + "px 0px");
+  player.sprite().css('background-position', -player.frameWidth * frame + "px 0px");
+  if (game.animating) setTimeout(function() { setFrame(player); }, 40);
 }
 
-function giveOompf(player) {
+function animate() {
   jopa.state[2] = jopa.state[2] + 1;
   asja.state[2] = asja.state[2] + 1;
+  if (game.animating) setTimeout(function() { animate(); }, game.animationInterval);
 }
 
 function generateTask(player) {
   var task = ["left", "up", "down", "right"][Math.floor(Math.random() * 4)];
   player.task = task;
-  player.area().find(".task").removeClass("true false").html(task);
-  setTimeout(function() { generateTask(player); }, Math.random() * (taskMax - taskMin) + taskMin);
+  player.area().find(".task").removeClass("hidden true false").html(task);
+  if (game.taskDifficulty >= 1) {
+    var taskPos = ["left", "right", "center", "bottom"][Math.floor(Math.random() * 4)];
+    player.area().find(".task").toggleClass(taskPos);
+  }
+  if (game.interacting) {
+    setTimeout(function() { generateTask(player); }, game.randomTaskLength());
+  } else {
+    $(".player-area .task").addClass("hidden").removeClass("left up down right");
+  }
+}
+
+function setHardening() {
+  // Speed up tasks every 7 seconds
+  var speedupInterval = setInterval(function() {
+    if (game.interacting) {
+      game.taskLength[0] = Math.max(200, game.taskLength[0] - 300);
+      game.taskLength[1] = Math.max(600, game.taskLength[1] - 300);
+      game.animationInterval = Math.max(60, game.animationInterval - 20);
+    } else {
+      clearInterval(speedupInterval);
+    }
+  }, 7000);
+  
+  // Change background color
+  setTimeout(changeBackground, 30000);
+  
+  // Make tasks moveable  
+  setTimeout(function() { game.taskDifficulty = 1 }, 54000);
+  
+  setTimeout(startConfetti, 74000)
+}
+
+function changeBackground() {
+  $("html").css("background-color", randomColor());
+  if (Math.random > 0.8) game.backgroundInterval = Math.max(100, game.animationInterval - 50);
+  if (game.interacting) setTimeout(changeBackground, game.backgroundInterval);
+}
+
+function randomColor() {
+  var color = [Math.random() * 256, Math.random() * 256, Math.random() * 256];
+  var mixColor = [255, 255, 255];
+  for (var i = 0; i < 3; i++) { color[i] = Math.floor((color[i] + mixColor[i]) / 2); }
+  return "rgb(" + color.join(",") + ")";
 }
 
 function checkKey(e) {
@@ -134,13 +217,72 @@ function checkControls(player, key) {
   
   if (player.task) {
     var correct = (playerDirection == player.task);
-    player.area().find(".task").addClass(correct ? "true" : "false");    
+    player.area().find(".task").addClass(correct ? "true" : "false");
+    if (correct) addPoints(player, 50);
     player.task = null;
   }
-    
-   
-  // Svatka tipka ciklira između grupe različitih poteza
-  
+}
+
+function addPoints(player, points) {
+  var from = {points: player.points};
+  var to = {points: player.points + points};
+  var $points = player.area().find(".points");
+  $(from).animate(to, {
+    duration: 500,
+    step: function() { $points.html(Math.round(this.points)); }
+  });
+  player.points = player.points + points;
 }
 
 
+// ======================== Game End ========================
+
+
+function endGame() {
+  game.interacting = false;
+  game.animationInterval = 320;
+  $("#game-over").html("Time's up!").removeClass("hidden");
+  $("html").css("background-color", "#373337");
+  document.onkeydown = null;
+  jopa.state = ["idle", 0, 0]; 
+  asja.state = ["idle", 0, 0];
+  if (jopa.points >= asja.points) {
+    setTimeout(birthdayBonus, 5000);
+    setTimeout(showWinner, 10000);
+  } else {
+    setTimeout(showWinner, 5000);
+  }
+}
+
+function birthdayBonus() {
+  asja.area().append("<div class='bonus'>Birthday bonus!</div>");
+  sounds["bonus"].play();
+  asja.area().find(".bonus").animate({bottom: 90}, 1000, "linear").animate({opacity: 0}, 1000, "swing")
+  addPoints(asja, jopa.points - asja.points + 1)
+}
+
+function showWinner() {
+  $("#game-over").html("Asja wins!").removeClass("hidden");
+  jopa.state = ["end", 0, 0];
+  asja.state = ["end", 0, 0];
+  asja.area().addClass("winning");
+  sounds["win"].play();
+  setTimeout(function() { 
+    showWithFlicker([$("#play-again")], 7, function() {
+      $("#play-again").one("click", restartGame);
+    });
+  }, 8000);
+}
+
+function restartGame() {
+  game.reset();
+  game.animating = false;
+  jopa.state = ["idle", 0, 0];
+  asja.state = ["idle", 0, 0];
+  $("#game-over, #play-again").addClass("hidden");
+  $(".player, .name, .controls").addClass("hidden");
+  $(".player-area").removeClass("winning");
+  jopa.points = asja.points = 0;
+  $(".points").addClass("hidden").html("0");
+  gameScreen();
+}
